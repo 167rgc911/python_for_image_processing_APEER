db6b3a652a4c45f1027aa338ee3a5513d965b784
rgc <rgc@apbg4.my.domain>
2021-02-03 18:04:36 +0900

    recode: some tree traversal

--- tutorial66a_need_for_data_normalization.py
+++ tutorial66a_need_for_data_normalization.py
@@ -44,6 +44,82 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
  
+#https://scikit-learn.org/stable/auto_examples/tree/plot_unveil_tree_structure.html#sphx-glr-auto-examples-tree-plot-unveil-tree-structure-py
+def tree_traversal(clf, X_set):
+
+    n_nodes = clf.tree_.node_count
+    children_left = clf.tree_.children_left
+    children_right = clf.tree_.children_right
+    feature = clf.tree_.feature
+    threshold = clf.tree_.threshold
+
+    node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
+    is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+    stack = [(0, 0)]  # start with the root node id (0) and its depth (0)
+    while len(stack) > 0:
+        # `pop` ensures each node is only visited once
+        node_id, depth = stack.pop()
+        node_depth[node_id] = depth
+
+        # If the left and right child of a node is not the same we have a split
+        # node
+        is_split_node = children_left[node_id] != children_right[node_id]
+        # If a split node, append left and right children and depth to `stack`
+        # so we can loop through them
+        if is_split_node:
+            stack.append((children_left[node_id], depth + 1))
+            stack.append((children_right[node_id], depth + 1))
+        else:
+            is_leaves[node_id] = True
+
+    print("The binary tree structure has {n} nodes and has "
+        "the following tree structure:\n".format(n=n_nodes))
+    for i in range(n_nodes):
+        if is_leaves[i]:
+            print("{space}node={node} is a leaf node.".format(
+                space=node_depth[i] * "\t", node=i))
+        else:
+            print("{space}node={node} is a split node: "
+                "go to node {left} if X[:, {feature}] <= {threshold} "
+                "else to node {right}.".format(
+                    space=node_depth[i] * "\t",
+                    node=i,
+                    left=children_left[i],
+                    feature=feature[i],
+                    threshold=threshold[i],
+                    right=children_right[i]))
+
+    node_indicator = clf.decision_path(X_test)
+    leaf_id = clf.apply(X_test)
+
+    sample_id = 0
+    # obtain ids of the nodes `sample_id` goes through, i.e., row `sample_id`
+    node_index = node_indicator.indices[node_indicator.indptr[sample_id]:
+                                        node_indicator.indptr[sample_id + 1]]
+
+    print('Rules used to predict sample {id}:\n'.format(id=sample_id))
+        for node_id in node_index:
+        # continue to the next node if it is a leaf node
+        if leaf_id[sample_id] == node_id:
+            continue
+
+        # check if value of the split feature for sample 0 is below threshold
+        if (X_test[sample_id, feature[node_id]] <= threshold[node_id]):
+            threshold_sign = "<="
+        else:
+            threshold_sign = ">"
+
+        print("decision node {node} : (X_test[{sample}, {feature}] = {value}) "
+              "{inequality} {threshold})".format(
+                node=node_id,
+                sample=sample_id,
+                feature=feature[node_id],
+                value=X_test[sample_id, feature[node_id]],
+                inequality=threshold_sign,
+                threshold=threshold[node_id]))
+
+    return
+
 df = pd.read_csv("ml/DATA/breast_cancer/wisconsin_breast_cancer_dataset.csv")
 
 print(df.describe().T)  #Values need to be normalized before fitting. 
